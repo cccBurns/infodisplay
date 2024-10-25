71aa783d11ddef1d36747b0dc803e4eb517951c4
christian burns <christianburns03@gmail.com>
2024-10-25 13:59:43 -0300

    viernes 25 13hs

--- inicio/static/inicio/scripts.js
+++ inicio/static/inicio/scripts.js
@@ -46,6 +46,8 @@ function mostrarDiaActual() {
     document.getElementById('current-day').textContent = `${diaSemana} ${diaNumero}`;
 }
 
+// ----------  CLIMA  ------------
+
 // Clave de API y ciudad
 const apiKey = '2abe0509728e9c6f401eb355c235c6c0';
 const ciudad = 'Buenos Aires';
@@ -61,10 +63,10 @@ async function obtenerClima() {
         const data = await response.json();
         
         // Elementos del clima actual
-        document.getElementById('temperature').textContent = `Ahora: ${Math.round(data.main.temp)}°C`;
-        document.getElementById('temp-max').textContent = `Máx: ${Math.round(data.main.temp_max)}°C`; // Mostrar temperatura máxima
-        document.getElementById('temp-min').textContent = `Mín: ${Math.round(data.main.temp_min)}°C`; // Mostrar temperatura mínima
-        document.getElementById('humidity').textContent = `Humedad: ${data.main.humidity}%`;
+        document.getElementById('temperature').textContent = `AHORA: ${Math.round(data.main.temp)}°C`;
+        document.getElementById('temp-max').textContent = `MAX: ${Math.round(data.main.temp_max)}°C`; // Mostrar temperatura máxima
+        document.getElementById('temp-min').textContent = `MIN: ${Math.round(data.main.temp_min)}°C`; // Mostrar temperatura mínima
+        document.getElementById('humidity').textContent = `HUMEDAD: ${data.main.humidity}%`;
         document.getElementById('weather-icon').src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 
         // Obtener el pronóstico
@@ -89,16 +91,20 @@ async function obtenerPronostico() {
         // Mostrar los pronósticos para los próximos días
         for (let i = 1; i <= 4; i++) {
             const pronostico = pronosticos[i * 8];
-            
+
+            // Obtener la fecha y el día de la semana
             const fecha = new Date(pronostico.dt * 1000);
-            const opciones = { weekday: 'long' };
-            const dia = fecha.toLocaleDateString('es-ES', opciones).toUpperCase();
-            document.getElementById(`hora-${i * 24}h`).textContent = dia;
+            const opcionesDia = { weekday: 'long' };
+            const dia = fecha.toLocaleDateString('es-ES', opcionesDia).toUpperCase();
+            const diaNumero = fecha.getDate(); // Obtener solo el número del día
+
+            // Actualizar el contenido del día y el número
+            document.getElementById(`hora-${i * 24}h`).textContent = `${dia} ${diaNumero}`;
 
             const maxTemp = Math.round(pronosticos.slice(i * 8 - 8, i * 8).reduce((max, p) => Math.max(max, p.main.temp_max), -Infinity));
             const minTemp = Math.round(pronosticos.slice(i * 8 - 8, i * 8).reduce((min, p) => Math.min(min, p.main.temp_min), Infinity));
             
-            document.getElementById(`temperature-${i * 24}h`).innerHTML = `Max: ${maxTemp}°C<br>Min: ${minTemp}°C`;
+            document.getElementById(`temperature-${i * 24}h`).innerHTML = `MAX: ${maxTemp}°C<br>MIN: ${minTemp}°C`;
             document.getElementById(`weather-icon-${i * 24}h`).src = `http://openweathermap.org/img/wn/${pronostico.weather[0].icon}@2x.png`;
         }
 
@@ -106,6 +112,8 @@ async function obtenerPronostico() {
         console.error('Error:', error);
     }
 }
+
+
 
 // Llamada inicial para obtener el clima y el día
 document.addEventListener('DOMContentLoaded', (event) => {
