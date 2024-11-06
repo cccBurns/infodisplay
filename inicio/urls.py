54f3efa1e7277f707f68c1f0f4f30ff57cd768d2
christian burns <christianburns03@gmail.com>
2024-11-06 10:38:00 -0300

    Miercoles 6

--- inicio/urls.py
+++ inicio/urls.py
@@ -3,5 +3,7 @@ from . import views
 from inicio.views import inicio
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),    
+    path('', views.inicio, name='inicio'),  
+    path('about/', views.about, name='about'), 
+    path('contacto/', views.contacto, name='contacto'),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
-def inicio(request):
-    
+def inicio(request):    
     return render(request, 'inicio/base.html', {})
+
+def about(request):
+    return render(request, 'inicio/about.html')
+
+def contacto(request):
+    return render(request, 'inicio/contacto.html')
